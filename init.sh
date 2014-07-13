d304de6be5deb33bd2cbfd1a3a6b8bf3506d92ed
ryo0301 <ryo0301@gmail.com>
2014-07-14 00:48:42 +0900

    vimのBundleInstallまで実行

--- init.sh
+++ init.sh
@@ -2,7 +2,17 @@
 
 root=$(cd $(dirname $0) && pwd)
 
+
 for src in $(find $root -name '*.symlink'); do
   dest=$HOME/.$(basename ${src%.*})
   [ -e $dest ] || ln -s $src $dest
 done
+
+
+mkdir -p $HOME/.vim/bundle
+
+if [ ! -d $HOME/bundle/vundle ]; then
+  git clone https://github.com/gmarik/Vundle.vim.git $HOME/.vim/bundle/vundle
+fi
+
+vim +BundleInstall +qall

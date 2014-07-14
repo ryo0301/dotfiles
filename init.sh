5b40f1674f332ccce2acbe32f3ffc017428ddf0c
ryo0301 <ryo0301@gmail.com>
2014-07-14 18:59:08 +0900

    vimの初期設定が動いてなかった

--- init.sh
+++ init.sh
@@ -2,7 +2,6 @@
 
 root=$(cd $(dirname $0) && pwd)
 
-
 for src in $(find $root -name '*.symlink'); do
   dest=$HOME/.$(basename ${src%.*})
   [ -e $dest ] || ln -s $src $dest
@@ -12,10 +11,10 @@ done
 bundle_root=$HOME/.vim/bundle
 vundle_dir=$bundle_root/vundle
 
-mkdir -p $vundle_dir
+mkdir -p $bundle_root
 
 if [ ! -d $vundle_dir ]; then
   git clone https://github.com/gmarik/Vundle.vim.git $vundle_dir
 fi
 
-vim +BundleInstall +qall
+vim +BundleInstall +qall!

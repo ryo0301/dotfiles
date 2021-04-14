c24ee6d068d0d5924c9d9a7308b0a504ff0d3546
ryo0301 <ryo0301@gmail.com>
2021-04-14 22:34:22 +0900

    元ファイルの階層に合わせてシンボリックリンクできるよう変更

--- init.sh
+++ init.sh
@@ -3,15 +3,16 @@
 root=$(cd $(dirname $0) && pwd)
 
 for src in $(find $root -name '*.symlink'); do
-  dest=$HOME/.$(basename ${src%.*})
+  dest=$HOME/.${src#$root/}
+  dest=${dest%.*}
   [ -e $dest ] || ln -s $src $dest
 done
 
 
-bundle_root=$HOME/.vim/bundle
-neobundle_dir=$bundle_root/neobundle.vim
-mkdir -p $bundle_root
-
-[ ! -d $neobundle_dir ] && git clone https://github.com/Shougo/neobundle.vim $neobundle_dir
-
-vim +NeoBundleInstall +qall!
+#bundle_root=$HOME/.vim/bundle
+#neobundle_dir=$bundle_root/neobundle.vim
+#mkdir -p $bundle_root
+#
+#[ ! -d $neobundle_dir ] && git clone https://github.com/Shougo/neobundle.vim $neobundle_dir
+#
+#vim +NeoBundleInstall +qall!

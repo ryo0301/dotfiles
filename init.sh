b1b616a49bca2e8b2212c4cb2747f5799c9d546a
ryo0301 <ryo0301@gmail.com>
2022-10-07 13:22:06 +0900

    defaultsの設定を追加

--- init.sh
+++ init.sh
@@ -34,4 +34,20 @@ case "$1" in
     fisher install 0rax/fish-bd
     fisher install jethrokuan/z
     ;;
+
+  def)
+    defaults write -g KeyRepeat -int 2
+    defaults write -g InitialKeyRepeat -int 15
+    defaults write -g com.apple.trackpad.scaling -int 3
+    defaults write -g AppleShowAllExtensions -bool true
+    defaults write com.apple.menuextra.clock DateFormat -string 'M\\U6708d\\U65e5(EEE)  H:mm:ss'
+    defaults write com.apple.menuextra.clock ShowSeconds -bool true
+    defaults write com.apple.AppleMultitouchTrackpad Dragging -bool true
+    defaults write com.apple.AppleMultitouchTrackpad DragLock -bool true
+    defaults write com.apple.finder AppleShowAllFiles -bool true
+    defaults write com.apple.dock wvous-br-corner -int 13
+    defaults write com.apple.dock wvous-br-modifier -int 0
+    killall Finder
+    killall Dock
+    ;;
 esac

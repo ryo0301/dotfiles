64470b051a7e2dcaf212667d4a93b132a6428edb
ryo0301 <ryo0301@gmail.com>
2022-11-20 17:16:20 +0900

    init.sh修正

--- init.sh
+++ init.sh
@@ -3,38 +3,6 @@
 root=$(cd $(dirname $0) && pwd)
 
 case "$1" in
-  link)
-    for src in $(find $root -name '*.symlink'); do
-      dest=$HOME/.${src#$root/}
-      dest=${dest%.*}
-      if [ ! -e $dest ];then
-        mkdir -p $(dirname $dest)
-        ln -s $src $dest
-      fi
-    done
-    ;;
-
-  brew)
-    brew bundle install --file "$root/Brewfile"
-    ;;
-
-  vim)
-    brew install vim
-    bundle_root=$HOME/.vim/bundle
-    neobundle_dir=$bundle_root/neobundle.vim
-    mkdir -p $bundle_root
-    [ ! -d $neobundle_dir ] && git clone https://github.com/Shougo/neobundle.vim $neobundle_dir
-    vim +NeoBundleInstall +qall!
-    ;;
-
-  fisher)
-    curl -sL https://git.io/fisher | source
-    fisher install jorgebucaran/fisher
-    fisher install oh-my-fish/plugin-peco
-    fisher install 0rax/fish-bd
-    fisher install jethrokuan/z
-    ;;
-
   def)
     defaults write -g KeyRepeat -int 2
     defaults write -g InitialKeyRepeat -int 15
@@ -49,5 +17,46 @@ case "$1" in
     defaults write com.apple.dock wvous-br-modifier -int 0
     killall Finder
     killall Dock
+    ;;
+
+  link)
+    for src in $(find $root -name '*.symlink'); do
+      dest=$HOME/.${src#$root/}
+      dest=${dest%.*}
+      if [ ! -e $dest ];then
+        mkdir -p $(dirname $dest)
+        ln -s $src $dest
+      fi
+    done
+    ;;
+
+  brew)
+    /opt/homebrew/bin/brew bundle install --file "$root/Brewfile"
+    ;;
+
+  vim)
+    /opt/homebrew/bin/brew install vim
+    bundle_root=$HOME/.vim/bundle
+    neobundle_dir=$bundle_root/neobundle.vim
+    mkdir -p $bundle_root
+    [ ! -d $neobundle_dir ] && git clone https://github.com/Shougo/neobundle.vim $neobundle_dir
+    vim +NeoBundleInstall +qall!
+    ;;
+
+  fisher)
+    /opt/homebrew/bin/fish -l -c "$(curl -sL https://git.io/fisher | source)"
+    fisher install jorgebucaran/fisher
+    fisher install oh-my-fish/plugin-peco
+    fisher install 0rax/fish-bd
+    fisher install jethrokuan/z
+    ;;
+  *)
+    set +x
+    clear
+    echo def
+    echo link
+    echo brew
+    echo vim
+    echo fisher
     ;;
 esac
